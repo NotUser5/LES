55a299c00abbcaf0cca87bf6d197a9bbd4186591
NotUser5 <jmtorrichelli@gmail.com>
2023-09-21 15:25:26 -0300

    Added some pastes and classes

---
+++ LES.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using LES.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LES.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
