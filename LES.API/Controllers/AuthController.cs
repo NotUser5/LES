b7d8d378ffec91f1f9cd22ef453358b7e9587927
NotUser5 <jmtorrichelli@gmail.com>
2023-10-17 14:07:51 -0300

    PBO-2 AuthController connected to the db

--- LES.API/Controllers/AuthController.cs
+++ LES.API/Controllers/AuthController.cs
@@ -1,7 +1,10 @@
 ﻿using LES.Domain.Models;
 using LES.Domain.ViewModels;
+using LES.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -14,33 +17,52 @@ namespace LES.API.Controllers
     {
         public static User user = new User();
         private readonly IConfiguration _configuration;
+        private readonly DataContext _dataContext;
 
-        public AuthController(IConfiguration configuration)
+        public AuthController(IConfiguration configuration, DataContext dataContext)
         {
             _configuration = configuration;
+            _dataContext = dataContext;
         }
 
         [HttpPost("register")]
         public ActionResult<User> Register(UserViewModel request)
         {
+            if (_dataContext.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("User already exists.");
+            }
+
             string passwordHash
                 = BCrypt.Net.BCrypt.HashPassword(request.Password);
 
-            user.Email = request.Email;
-            user.PasswordHash = passwordHash;
+            User newUser = new User
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Password = passwordHash,
+                Cpf = request.Cpf,
+                Birthdate = request.Birthdate
+            };
 
-            return Ok(user);
+            _dataContext.Users.Add(newUser);
+            _dataContext.SaveChanges();
+
+            return Ok(newUser);
         }
 
         [HttpPost("login")]
         public ActionResult<User> Login(UserViewModel request)
         {
+            var user = _dataContext.Users.FirstOrDefault(u => u.Email == request.Email);
+
             if (user.Email != request.Email)
             {
                 return BadRequest("User not found.");
             }
 
-            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.Password))
             {
                 return BadRequest("Wrong password.");
             }
---
+++ LES.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using LES.Domain.Models;
+using LES.Infrastructure.Data;
+using LES.Infrastructure.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LES.Infrastructure.Repository
+{
+    public class UserRepository : RepositoryBase<Category, Guid>, IUserRepository
+    {
+        public UserRepository(DataContext applicationDataContext) : base(applicationDataContext) { }
+    }
+}
